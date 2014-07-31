dce8eb7fbaa094e28deb1b402cf628bdaa2a29ba
Qosdil A <qosdil@gmail.com>
2014-07-31 10:40:46 +0700

    AdminController -> Controller. Error fixed.

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 Route::group(array('prefix' => Admin::getUrlPrefix()), function()
 {
-	Route::get('/', 'Webarq\Admin\AdminController@getIndex');
+	Route::get('/', 'Webarq\Admin\Controller@getIndex');
 
 	Route::controller('auth', 'Webarq\Admin\AuthController');
 	Route::controller('email/template', 'Webarq\Admin\Email\TemplateController');

54952336f049c21874f2177c2c92696ef8f27d1d
Qosdil A <qosdil@gmail.com>
2014-11-17 23:46:01 +0700

    Redirect to login and show a message if a frontend user is logged in.

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -3,8 +3,16 @@
 class AuthController extends Controller {
 	
 	public function getLogin()
-	{	
-		\Auth::logout();
+	{
+		if (\Auth::check() && ! \Auth::user()->admin) // A frontend user is logged in
+		{
+			$this->createMessage('A frontend user ('.\Auth::user()->email.') is currently logged in. To continue logging in to this admin panel, please consider using another browser or else the frontend session will be deleted.', 'warning', false);
+		}
+		else
+		{
+			\Auth::logout();
+		}
+
 		$this->layout->isLoginPage = true;
 	}
 	
